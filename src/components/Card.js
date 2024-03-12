fda8bdb3ed0cf16e24f388048d87c8ac7c25edfb
Flavian <flahribu@gmail.com>
2024-03-12 18:02:48 +0100

    Initial commit

---
+++ src/components/Card.js
@@ -0,0 +1,37 @@
+import { Center, Heading, HStack, Image, Text, VStack } from "@chakra-ui/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import React from "react";
+
+const Card = ({ title, description, imageSrc }) => {
+
+
+
+  return (
+    <VStack
+     color='black'
+     backgroundColor='white'
+     cursor='pointer'
+     borderRadius='15px'>
+
+      <Image src={imageSrc}  borderRadius='15px' />
+      <VStack spacing= {4} p={4} alignItems='flex-start'>
+        <HStack justifyContent='space-between' alignItems='center'>
+          <Heading as='h3' size='md' >{title}</Heading >
+        </HStack>
+        <Text color='#64748b' fontSize='lm'>{description}</Text>
+        <HStack>
+          <p>See more</p>
+          <FontAwesomeIcon icon={faArrowRight} size="1x" />
+        </HStack>
+      </VStack>
+    </VStack>
+  )
+};
+
+export default Card;
+
+
+// Implement the UI for the Card component according to the instructions.
+  // You should be able to implement the component with the elements imported above.
+  // Feel free to import other UI components from Chakra UI if you wish to.
---
+++ src/components/LandingSection.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Avatar, Heading, VStack } from "@chakra-ui/react";
+import FullScreenSection from "./FullScreenSection";
+
+const greeting = "Hello, I am Pete!";
+const bio1 = "A frontend developer";
+const bio2 = "specialised in React";
+
+// Implement the UI for the LandingSection component according to the instructions.
+// Use a combination of Avatar, Heading and VStack components.
+const LandingSection = () => (
+  <FullScreenSection
+    justifyContent="center"
+    alignItems="center"
+    isDarkBackground
+    backgroundColor="#2A4365"
+  >
+    <VStack spacing={6}>
+      <Avatar src="https://i.pravatar.cc/150?img=7 "
+        size='2x1'
+        name='Your Name'
+
+      />
+      {/*Im  using here  Chakra Ui */ }
+      <Heading as='h4' size='md' noOfLines={1}> {greeting} </Heading>
+
+      <VStack spacing={2}>
+        <Heading as='h1'  noOfLines={1}> {bio1} </Heading>
+        <Heading as='h1'  noOfLines={1}> {bio2} </Heading>
+      </VStack>
+       
+    </VStack>
+
+  </FullScreenSection>
+);
+
+export default LandingSection;
